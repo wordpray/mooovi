b2051c24e85512a2e45b172691857c5598ce4046
y.yoshiki <labiatejoker@gmail.com>
2017-06-14 18:39:53 +0900

    Initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141004013909) do
-
-  create_table "products", force: true do |t|
-    t.string   "title"
-    t.text     "image_url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "reviews", force: true do |t|
-    t.string   "nickname"
-    t.integer  "rate"
-    t.text     "review"
-    t.integer  "product_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
